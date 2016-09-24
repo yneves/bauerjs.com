a6c20ce19ce49db02f057546ed40235c02baa868
Yuri Neves Silveira <yurinevessilveira@gmail.com>
2016-09-24 13:55:20 -0300

    update

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // - -------------------------------------------------------------------- - //
 
 module.exports = function() {
-  
+
   var api = {};
   var parseAPI = require("./src/parse-api.js");
   var renderAPI = require("./src/render-api.js");
@@ -9,7 +9,7 @@ module.exports = function() {
   var renderIndex = require("./src/render-index.js");
   var compileLESS = require("./src/compile-less.js");
   var listModules = require("./src/list-modules.js");
-  
+
   return this.Promise
     .print("Build started")
     .then(listModules)
@@ -21,22 +21,9 @@ module.exports = function() {
     .then(renderAPI)
     .then(renderIndex)
     .then(compileLESS)
-    
+
     .print("Build finished")
-    .rsync({
-      source: __dirname + "/www/*",
-      destination: "root@107.170.69.117:/home/scripts/bauerjs/www",
-      exclude: [".git"],
-      flags: "avr",
-      shell: "ssh -2 -p 22  -i " + __dirname + "/../id_rsa"
-    })
-    .print("Deployed")
-    .watch([
-      __dirname + "/../less",
-      __dirname + "/../html"
-    ])
-    .print("File changed.")
-    .repeat(true);
+    .exit();
 };
 
 // - -------------------------------------------------------------------- - //
